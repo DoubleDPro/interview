baf44ffc8a19723906df0835706b358830429d1f
Dmitry Usachev <vizant88@gmail.com>
2024-08-03 14:11:10 +0300

    Added crossorigin for test create method
    To have ability receiving requests from frontend added annotation
    @crossorigin for test create method

--- backend/tests-service/src/main/java/ru/usachev/interview/backend/testsservice/controllers/TestTaskRestController.java
+++ backend/tests-service/src/main/java/ru/usachev/interview/backend/testsservice/controllers/TestTaskRestController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ public class TestTaskRestController {
     this.service = service;
   }
 
+  @CrossOrigin
   @PostMapping(
       value = "create",
       consumes = MediaType.APPLICATION_JSON_VALUE,
